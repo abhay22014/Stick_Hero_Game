d0feaec98f4fdc2dd98b105846c962399170b428
abhay22014 <abhay22014@iiitd.ac.in>
2024-07-10 11:21:56 +0530

    Initial commit

---
+++ src/main/java/com/example/stickhero/Movement.java
@@ -0,0 +1,11 @@
+package com.example.stickhero;
+public interface Movement {
+    public void moveUp();
+    public void moveDown();
+    public void moveRight();
+
+
+    public void flip();
+    public void jump();
+
+}
